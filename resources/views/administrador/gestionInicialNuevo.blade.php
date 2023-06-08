9ef09b2db0085f5869ed8df6a9ddb3e9fbacc82b
John Alexander Lagos <john.lagos@codess.org.co>
2023-06-08 12:03:36 -0500

    prueba final m

--- resources/views/administrador/gestionInicialNuevo.blade.php
+++ resources/views/administrador/gestionInicialNuevo.blade.php
@@ -3,7 +3,7 @@
 @section('content_header') 
     <div class='row mb-2'>
         <div class='col-sm-6'>
-            <h1>HOLA</h1>
+            <h1>HOLA SOY MAURO</h1>
         </div>
     </div>
 @stop
