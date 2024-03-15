4c6cd149f837dfd232357ae00987bef70eca52c3
Mauro Ramirez <mauro.ramirez@codess.org.co>
2024-03-14 21:54:25 -0500

    Jl-Actuali

--- app/Http/Controllers/Coordinador/CalificacionJuntasController.php
+++ app/Http/Controllers/Coordinador/CalificacionJuntasController.php
@@ -304,6 +304,7 @@ class CalificacionJuntasController extends Controller
                 ['sipc.Id_cliente', '=', $id_cliente],
                 ['sipc.Id_proceso', '=', $request->Id_proceso],
                 ['sipc.Servicio_asociado', '=', $request->Id_servicio],
+                ['sipc.Modulo_principal', '=', 'Si'],
                 ['sipc.Status_parametrico', '=', 'Activo']
             ])->get();
 
